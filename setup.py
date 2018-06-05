f62dcc13e30e655ed6c00b999445b7176d7c02bf
John Vivian <jtvivian@gmail.com>
2018-06-05 12:24:44 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='rnaseq-lib3',
+      version='1.0a1',
+      description='Library of convenience functions related to current research',
+      url='http://github.com/jvivian/rnaseq-lib3',
+      author='John Vivian',
+      author_email='jtvivian@gmail.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=find_packages('src'))
