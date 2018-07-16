49beaca6f34c0dad7e692d2d03da56a30a4ba407
John Vivian <jtvivian@gmail.com>
2018-07-16 12:50:34 -0700

    Add other normalization functions

--- src/rnaseq_lib3/math/__init__.py
+++ src/rnaseq_lib3/math/__init__.py
@@ -1,6 +1,23 @@
 import numpy as np
+from pandas import DataFrame
 
 
+# Normalization
 def l2norm(x: float, pad: float = 1.0) -> float:
     """Log2 normalization function"""
     return np.log2(x + pad)
+
+
+def min_max_normalize(df: DataFrame) -> DataFrame:
+    """Rescale features to the range of [0, 1]"""
+    return (df - df.min()) / (df.max() - df.min())
+
+
+def mean_normalize(df: DataFrame) -> DataFrame:
+    """Normalizes data to mean of 0 and std of 1"""
+    return (df - df.mean()) / df.std()
+
+
+def softmax(df: DataFrame) -> DataFrame:
+    """Normalizes columns to sum to 1"""
+    return df.divide(df.sum())
