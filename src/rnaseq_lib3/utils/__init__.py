6de78a214c8573fa884cf1a56a11b9dc558615c4
John Vivian <jtvivian@gmail.com>
2018-08-16 10:49:57 -0700

    Add typing

--- src/rnaseq_lib3/utils/__init__.py
+++ src/rnaseq_lib3/utils/__init__.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def rget(url, params=None) -> requests.models.Response:
+def rget(url: str, params: dict = None) -> requests.models.Response:
     """requests.get wrapper that checks status code for 200 or returns None"""
     r = requests.get(url, params=params)
     if r.status_code != 200:
