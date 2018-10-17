7c544bc2ce63534d6392e9822269fb7237e26a60
John Vivian <jtvivian@gmail.com>
2018-10-17 12:43:42 -0700

    Add support for single-end and add typing

--- src/rnaseq_lib3/fastq/__init__.py
+++ src/rnaseq_lib3/fastq/__init__.py
@@ -1,4 +1,5 @@
 import gzip
+from typing import Tuple
 
 from tqdm import tqdm
 
@@ -6,7 +7,7 @@ from tqdm import tqdm
 # Adopted from: https://github.com/linsalrob/EdwardsLab/blob/master/bin/pair_fastq_fast.py
 
 
-def pair_fastq(r1_path, r2_path):
+def pair_fastq(r1_path: str, r2_path: str, output_singles: bool = False) -> None:
     # read the first file into a data structure
     seqs = {}
     for seqid, header, seq, qual in tqdm(stream_fastq(r1_path)):
@@ -14,9 +15,10 @@ def pair_fastq(r1_path, r2_path):
         seqs[seqid] = [header, seq, qual]
 
     lp = open("{}.paired.fastq".format(r1_path.replace('.fastq', '').replace('.gz', '')), 'w')
-    # lu = open("{}.singles.fastq".format(args.l.replace('.fastq', '').replace('.gz', '')), 'w')
     rp = open("{}.paired.fastq".format(r2_path.replace('.fastq', '').replace('.gz', '')), 'w')
-    # ru = open("{}.singles.fastq".format(args.r.replace('.fastq', '').replace('.gz', '')), 'w')
+    if output_singles:
+        lu = open("{}.singles.fastq".format(args.l.replace('.fastq', '').replace('.gz', '')), 'w')
+        ru = open("{}.singles.fastq".format(args.r.replace('.fastq', '').replace('.gz', '')), 'w')
 
     # read the first file into a data structure
     seen = set()
@@ -26,20 +28,20 @@ def pair_fastq(r1_path, r2_path):
         if seqid in seqs:
             lp.write("@" + seqs[seqid][0] + "\n" + seqs[seqid][1] + "\n+\n" + seqs[seqid][2] + "\n")
             rp.write("@" + header + "\n" + seq + "\n+\n" + qual + "\n")
-        # else:
-        #    ru.write("@" + header + "\n" + seq + "\n+\n" + qual + "\n")
+        elif output_singles:
+            ru.write("@" + header + "\n" + seq + "\n+\n" + qual + "\n")
 
-    # for seqid in seqs:
-    #    if seqid not in seen:
-    #        lu.write("@" + seqs[seqid][0] + "\n" + seqs[seqid][1] + "\n+\n" + seqs[seqid][2] + "\n")
-
+    if output_singles:
+        for seqid in seqs:
+            if seqid not in seen:
+                lu.write("@" + seqs[seqid][0] + "\n" + seqs[seqid][1] + "\n+\n" + seqs[seqid][2] + "\n")
+        lu.close()
+        ru.close()
     lp.close()
-    # lu.close()
     rp.close()
-    # ru.close()
 
 
-def stream_fastq(fqfile):
+def stream_fastq(fqfile: str) -> Tuple(str, str, str, str):
     """Read a fastq file and provide an iterable of the sequence ID, the
     full header, the sequence, and the quaity scores.
 
