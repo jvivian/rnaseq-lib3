04d8d3c72ae5f18dc9f7528a9d156488044d59e1
John Vivian <jtvivian@gmail.com>
2018-10-25 13:42:19 -0700

    EC2 startup script

---
+++ src/scripts/ec2-startup.sh
@@ -0,0 +1,18 @@
+# System updates and install
+sudo apt-get update
+#sudo apt -y upgrade
+sudo apt-get install -y python3-dev build-essential git python3-pip
+
+# Personal library
+git clone https://github.com/jvivian/rnaseq-lib3
+pip3 install --user -e rnaseq-lib3
+pip3 install --user boto3
+
+# aws creds
+mkdir ~/.aws
+
+# SSD Mount
+sudo mkfs -t xfs /dev/nvme0n1
+sudo mkdir /data
+sudo mount -t xfs /dev/nvme0n1 /data
+sudo chown ubuntu:ubuntu /data
