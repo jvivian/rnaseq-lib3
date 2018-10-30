fbe1461700f39835b62f1495a50ff012f9559e7d
John Vivian <jtvivian@gmail.com>
2018-10-30 10:15:05 -0700

    Move to CCLE-processing subdir

---
+++ src/scripts/CCLE-processing/process_ccle_autoscaling.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Vars
+ZONE=us-west-2a
+CLUSTER_NAME=jvivian-ccle-processing
+
+# Launch leader of cluster
+toil launch-cluster ${CLUSTER_NAME} --keyPairName jtvivian@gmail.com --leaderNodeType t2.medium --zone ${ZONE}
+
+# Copy workflow to leader
+toil rsync-cluster --zone ${ZONE} ${CLUSTER_NAME} process_ccle_toil.py :/root
+
+# SSH into leader
+toil ssh-cluster --zone ${ZONE} jvivian-ccle-processing
+
+# Launch once on the leader
+python /root/process_ccle_toil.py aws:us-west-2:jvivian-ccle-jobstore --provisioner aws \
+        --nodeTypes r4.4xlarge --maxNodes 5 --nodeStorage 150 --batchSystem mesos
