7c35c4aa91c915d0aeb68b13f1abc509d9752942
John Vivian <jtvivian@gmail.com>
2018-11-13 11:30:10 -0800

    Reorder utils and add tarball_files

--- src/rnaseq_lib3/utils/__init__.py
+++ src/rnaseq_lib3/utils/__init__.py
@@ -1,22 +1,19 @@
 import os
+import tarfile
 from subprocess import check_call
+from typing import List, Optional
 
 import requests
 
 
-def rget(url: str, params: dict = None) -> requests.models.Response:
+# Web / Downloading
+def rget(url: str, params: dict = None) -> Optional[requests.models.Response]:
     """requests.get wrapper that checks status code for 200 or returns None"""
     r = requests.get(url, params=params)
     if r.status_code != 200:
         return None
     else:
         return r
-
-
-def rreplace(s, old, new, occurrence):
-    """https://stackoverflow.com/users/230454/mg"""
-    li = s.rsplit(old, occurrence)
-    return new.join(li)
 
 
 def curl(url: str, work_dir: str = None):
@@ -30,3 +27,29 @@ def curl(url: str, work_dir: str = None):
     else:
         check_call(['curl', '-fs', '--retry', '5', url, '-o', file_path])
     return file_path
+
+
+# Files
+def tarball_files(tar_name: str, file_paths: List[str], output_dir: str = '.', prefix: str = ''):
+    """
+    Creates a tarball from a group of files
+
+    Args:
+        tar_name: Name of tarball
+        file_paths: Absolute file paths to include in the tarball
+        output_dir: Output destination
+        prefix: Optional prefix for files inside the tarball
+    """
+    with tarfile.open(os.path.join(output_dir, tar_name), 'w:gz') as f_out:
+        for file_path in file_paths:
+            if not file_path.startswith('/'):
+                raise ValueError('Path provided is relative not absolute.')
+            arcname = prefix + os.path.basename(file_path)
+            f_out.add(file_path, arcname=arcname)
+
+
+# Naming operations
+def rreplace(s, old, new, occurrence):
+    """https://stackoverflow.com/users/230454/mg"""
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
