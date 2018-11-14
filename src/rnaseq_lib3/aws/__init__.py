059b43e9ab83a51c5a7177974c84cf0554876a9b
John Vivian <jtvivian@gmail.com>
2018-11-13 19:31:23 -0800

    Add function to list running instances

--- src/rnaseq_lib3/aws/__init__.py
+++ src/rnaseq_lib3/aws/__init__.py
@@ -27,3 +27,18 @@ def delete_bucket(bucket_name, profile_name='default'):
     bucket_objs = bucket.objects.all()
     bucket_objs.delete()
     bucket.delete()
+
+
+# EC2
+# Adapted from: https://gist.github.com/mda590/679aba60ca03699d5b12a32314debdc0
+def list_running_instances(profile_name='default', region_name='us-west-2'):
+    session = boto3.session.Session(profile_name=profile_name)
+    ec2 = session.resource('ec2', region_name=region_name)
+
+    # Define running filters
+    filters = [{'Name': 'instance-state-name',
+                'Values': ['running']}]
+
+    instances = ec2.instances.filter(Filters=filters)
+    for instance in instances:
+        print(instance.id)
