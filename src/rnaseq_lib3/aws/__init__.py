b7f1f2da0b9e1ca0b23e617020f46b4c94b472b2
John Vivian <jtvivian@gmail.com>
2018-12-06 00:00:11 -0800

    Store and return keys
    Add some typing elsewhere

--- src/rnaseq_lib3/aws/__init__.py
+++ src/rnaseq_lib3/aws/__init__.py
@@ -4,20 +4,23 @@ import boto3
 
 
 # S3
-def list_all_buckets(profile_name: str='default'):
+def list_all_buckets(profile_name: str = 'default'):
     session = boto3.session.Session(profile_name=profile_name)
     s3 = session.resource('s3')
     for bucket in s3.buckets.all():
         print(bucket)
 
 
-def list_bucket(bucket_name, profile_name='default'):
+def list_bucket(bucket_name: str, profile_name: str = 'default'):
     session = boto3.session.Session(profile_name=profile_name)
     s3 = session.resource('s3')
 
+    keys = []
     bucket = s3.Bucket(bucket_name)
     for obj in bucket.objects.all():
-        print(obj)
+        print(obj.key)
+        keys.append(obj)
+    return keys
 
 
 def delete_bucket(bucket_name, profile_name='default'):
